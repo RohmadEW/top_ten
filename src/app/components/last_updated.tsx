cc9156e215523a54ea9b23b9b3efc82b88195590
Rohmad Eko Wahyudi <rohmad.ew@gmail.com>
2024-07-18 21:15:38 +0700

    fix: last updated

--- src/app/components/last_updated.tsx
+++ src/app/components/last_updated.tsx
@@ -1,5 +1,10 @@
 const LastUpdated = () => {
-  return <button className="btn font-bold"><span>Last Updated:</span> <span className="text-red-500">July 17, 2024</span></button>;
+  return (
+    <button className="btn font-bold text-[19px]">
+      <span className="tracking-wide">Last Updated:</span>{" "}
+      <span className="tracking-wide text-red-500">July 17, 2024</span>
+    </button>
+  );
 };
 
 export default LastUpdated;
