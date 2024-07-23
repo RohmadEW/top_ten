999fcf197f204b13df43afaa0a43b23231f4e63d
Rohmad Eko Wahyudi <rohmad.ew@gmail.com>
2024-07-23 10:21:43 +0700

    feat: Add conditional check for dummy data in getProducts API

--- src/api/getProducts.ts
+++ src/api/getProducts.ts
@@ -1,4 +1,5 @@
 import { apiClient } from "@/api/api-client";
+import { IS_DEVELOPMENT } from "@/lib/constants";
 import { ProductMetaData } from "@/types/product";
 import { Response } from "@/types/response";
 import axios from "axios";
@@ -8,8 +9,8 @@ type ProductsResponse = Response<ProductMetaData>;
 export const getProducts = async () => {
   let result = await apiClient.get<ProductsResponse>(``);
 
-  // if got error, get from dummy data
-  if (result.request.status !== 200)
+  // if got error, get from dummy data (only in development)
+  if (IS_DEVELOPMENT && result.request.status !== 200)
     result = await axios.get<ProductsResponse>("/api");
 
   return result.data.data;
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const IS_DEVELOPMENT = process.env.NODE_ENV !== "production";
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
